2724f0e606baf80d1df5f44126ac44d370346e35
Thomas Miconi <thomas.miconi@gmail.com>
2017-07-16 21:39:06 -0700

    Imperfect gradient over w only with plasticity
    Computes the gradient of y (long term) over w, with plasticity. The
    gradients are much more approximate than without plasticity, maybe
    because of a bug.

--- net.py
+++ net.py
@@ -2,8 +2,8 @@ import numpy as np
 
 np.random.seed(0)
 
-NBSTEPS = 100
-NBNEUR = 50
+NBSTEPS = 20 
+NBNEUR = 30
 ETA = .90
 EPSILONW = .001
 EPSILONY = .0
@@ -21,7 +21,7 @@ generaltgt = np.ones(NBNEUR)[:,None]  # Column vector
 
 
 
-alpha.fill(0)  # For now, no plasticity
+#alpha.fill(0)  # For now, no plasticity
 
 
 
@@ -32,16 +32,32 @@ wchange = np.random.randn(NBNEUR, NBNEUR) * .0001
 
 
 for numrun in range(3):
-    xs=[]; ys=[]; errs=[]; tgts = []; yprevs = []; weffs=[]
+    xs=[]; ys=[]; errs=[]; tgts = []; yprevs = [] 
     y = yinit.copy()
     yprev = np.zeros_like(y); yprevprev = np.zeros_like(y)
-    weff = w.copy()
     hebb.fill(0)
-    dykdwba = np.zeros((NBNEUR, NBNEUR, NBNEUR))
+    dykdwba = np.zeros((NBNEUR, NBNEUR, NBNEUR))  # dykdwba[k, b, a] = dy_k/dWba
+    dhkjdwba = np.zeros((NBNEUR, NBNEUR, NBNEUR, NBNEUR)) # dhkjdwba[k, j, b, a] = dh_kj/dWba
+    dykdwbaprev = dykdwba.copy()
 
     w[1,3] += EPSILONW
 
     for numstep in range(NBSTEPS): 
+
+        
+        # Now we compute dH_kj(t)/dWba = ETA * dH_kj(t-1)/dWba + (1-ETA) [ dy_k(t-1)/dWba * y_j(t-2) + dy_j(t-2)/dWba * y_k(t-1) ]
+        # (Reminder: at this point, y is really y(t-1) and yprev is really y(t-2); y(t) is computed below in the loop body)
+
+        tmpdout1 = dhkjdwba 
+        tmpdout2 = np.transpose(dykdwba[:,:,:,np.newaxis] * yprev, [0, 3, 1, 2])   # yprev_j * dy_k/dWba  
+        tmpdout3 = np.transpose(dykdwbaprev[:,:,:,np.newaxis] * y , [3, 0, 1, 2]) #  y_k * dyprev_j/dWba  
+
+        dhkjdwba = ETA * tmpdout1 + (1.0 - ETA) * (tmpdout2 + tmpdout3)
+
+        # We compute hebb(t) now, because we need it for dxkdwba.
+        hebb = ETA * hebb + (1.0 - ETA) * np.outer(y, yprev) # Note: At this stage, y is really y(t-1) and yprev is really y(t-2)
+
+        # Now we compute dx_k(t)/dWba (we use this to compute dy_k(t)/dWba after y(t) is computed)
 
         tmpdout = np.tensordot(w, dykdwba, ([1], [0]))  # Somehow this works
         # Equivalent, slow way of doing the same thing:
@@ -50,25 +66,28 @@ for numrun in range(3):
         #        for k in range(NBNEUR):
         #            for j in range(NBNEUR):
         #                dout[k, b, a] += w[k, j] * dykdwba[j, b,a]
-        dxkdwba = tmpdout 
         # Special case when k=b: 
         for b in range(NBNEUR):
             for a in range(NBNEUR):
-                dxkdwba[b,b,a] += y[a]
-        
-        # Now dxkdwba = dx_k(t)/dW_ba
-        # To get dy_k(t)/dW_ba we just need to pass it through the tanh, which
-        # we do after the y(t) are computed, below.
+                tmpdout[b,b,a] += y[a]
+
+        # Now tmpdout = dx_k(t)/dW_ba - neglecting plasticity !
+        # Now to add in plasticity effects...
+        tmpdout1 = np.tensordot( (alpha * hebb), dykdwba, ([1], [0]) )  # Sum_j{ alpha_kj hebb_kj dy_j(t-1)/dWba } (very similar to the computation with w above)
+        tmpdout2 = np.tensordot( (alpha * dhkjdwba), y, ([1], [0]) ) # Sum_j{ alpha_kj dhebb_kj(t)/dWba y_j }
 
         
-        hebb = ETA * hebb + (1.0 - ETA) * np.outer(y, yprev) # Note: At this stage, y is really y(t-1) and yprev is really y(t-2)
-        weff = w.copy()
+        dxkdwba = tmpdout + tmpdout1 + tmpdout2
 
+        # To get dy_k(t)/dW_ba we just need to pass dxkdwba through the tanh
+        # nonlinearity, which we do after the y(t) are computed, below.
+        
         x = (w + alpha * hebb).dot(y)
         yprevprev = yprev.copy()
         yprev = y.copy()
         y = np.tanh(x)  # Finally computing y(t)
 
+        dykdwbaprev = dykdwba.copy()
         dykdwba = (dxkdwba.T * (1.0 - y*y)).T  # Broadcasting along the last two dimensions to account for the tanh
 
         if numrun == 1: #and numstep == 0:
@@ -77,7 +96,7 @@ for numrun in range(3):
             pred_dydwba = dykdwba.copy()
 
 
-        ys.append(y); tgts.append(generaltgt); yprevs.append(yprev); xs.append(x); weffs.append(weff)
+        ys.append(y); tgts.append(generaltgt); yprevs.append(yprev); xs.append(x); 
             
     finalxs.append(x)
     finalys.append(y)
